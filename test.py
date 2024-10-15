481625b044bb2dba99314f306bc02d82d9d5552a
sepi <mahdirashidiminimal@gmail.com>
2024-10-15 18:07:38 +0330

    RecycleView updated

--- test.py
+++ test.py
@@ -1,18 +1,11 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
 from repository import Repository
-from kivy.uix.label import Label
-from kivymd.uix.list import OneLineListItem
-from kivymd.uix.recycleview import RecycleView
-from kivy.properties import ListProperty
-
-
 
 repo = Repository()
 class RootScreen(Screen):
     pass
+
 
 class AddTaskScreen(Screen):
     def parse_and_submit(self):
@@ -24,6 +17,7 @@ class AddTaskScreen(Screen):
             repo.add_task(task_title, time_spent, difficulty)
             string = str(f"task {task_title} updated to database")
             self.ids.submit_message.text = string
+            #TODO make text green
 
             # Clear the input fields after submission (optional)
             self.ids.task_title.text = ""
@@ -31,26 +25,17 @@ class AddTaskScreen(Screen):
             self.ids.difficulty.text = "difficulty level"
 
 class TasksScreen(Screen):
-    tasks = repo.get_all_tasks()
-    pass
-
-
-
+    
+    def on_pre_enter(self):
+        task_recycle_view = self.ids.task_recycle_view
+        task_recycle_view.data = [{'text': f"{task[1]} took {task[2]} minutes"} for task in repo.get_all_tasks()]
 
 class MyScreenManager(ScreenManager):
     pass
 
-class TaskRecycleView(RecycleView):
-    def __init__(self, **kwargs):
-        super(TaskRecycleView, self).__init__(**kwargs)
-        self.data = [{'text': task[1]} for task in repo.get_all_tasks()]
-
-
 class MyTestApp(MDApp):
     def build(self):
-        TaskRecycleView()
         return MyScreenManager()
-    
 
 
 if __name__ == '__main__':
